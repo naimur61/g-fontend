af75dc1b9f6a84d8738202e4fc7bc4f153140d6a
Naimur <naimur.r61n@gmail.com>
2023-10-24 22:07:10 +0600

    incomplete

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,42 +1,70 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import Loader from "./Loader";
 
 const Input = () => {
+	const [open, setOpen] = React.useState(false);
+	const {
+		handleSubmit,
+		register,
+
+		reset,
+	} = useForm();
+
+	const onSubmit = async (data) => {
+		setOpen(true);
+		try {
+			const response = await axios.post("http://localhost:5000/email", {
+				data,
+			});
+			if (response.data.result.acknowledged === true) {
+				setOpen(false);
+				alert(response.data.massage);
+			}
+		} catch (error) {
+			alert("Error:", error);
+			setOpen(false);
+		}
+	};
+
 	return (
 		<Box>
-			{/* <TextField
-				sx={{
-					backgroundColor: "white",
-					border: "none",
-					":focus": { outline: "none" },
-				}}
-			/> */}
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<Box
+					component="input"
+					required
+					type="email"
+					sx={{
+						backgroundColor: "white",
+						border: "none",
+						":focus": { outline: "none" },
+						width: "80%",
+						padding: "1rem",
+						borderRadius: "2rem",
+						boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+						fontWeight: "bold",
+					}}
+					placeholder="Enter your email"
+					{...register("email")}
+				/>
 
-			<Box
-				component="input"
-				sx={{
-					backgroundColor: "white",
-					border: "none",
-					":focus": { outline: "none" },
-					width: "80%",
-					padding: "1rem",
-					borderRadius: "2rem",
-					boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
-					fontWeight: "bold",
-				}}
-				placeholder="Enter your email"
-			/>
-			<Button
-				variant="contained"
-				sx={{
-					backgroundImage: "linear-gradient(to right, rgb(83 101 245) 0%, rgb(166 109 239) 100%)",
-					borderRadius: "20px",
-					position: "absolute",
-					margin: "5px -7.2rem",
-				}}
-			>
-				Notify me
-			</Button>
+				<Button
+					variant="contained"
+					type="submit"
+					sx={{
+						backgroundImage: "linear-gradient(to right, rgb(83 101 245) 0%, rgb(166 109 239) 100%)",
+						borderRadius: "20px",
+						position: "absolute",
+						margin: "5px -7.2rem",
+					}}
+				>
+					Notify me
+				</Button>
+			</form>
+			<Loader open={open} />
 		</Box>
 	);
 };
