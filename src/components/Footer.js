8dfee581d0794b3b3fd6638a799733dc08d7f89a
Naimur <naimur.r61n@gmail.com>
2023-10-24 14:16:35 +0600

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,9 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 const Footer = () => {
 	return (
 		<Box mt="2rem" mx="auto" maxWidth="700px" textAlign="center" pt="3rem">
-			<Typography variant="h3" fontWeight="bold">
+			<hr style={{ width: "200px", margin: "0 auto" }} />
+
+			<Typography variant="h3" fontWeight="bold" mt="2rem">
 				Join our journey
 			</Typography>
 			<Typography variant="h5" fontWeight="bold" my="1rem">
